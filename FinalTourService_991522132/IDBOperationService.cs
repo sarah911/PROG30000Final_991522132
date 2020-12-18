08ca023dad5d64cee08d8556181db2d3e06ea8e1
Suzi Ryu <ryusu@ryusu@sheridancollege.ca>
2020-12-18 15:06:42 -0500

    Created Files and updates

--- FinalTourService_991522132/IDBOperationService.cs
+++ FinalTourService_991522132/IDBOperationService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace FinalTourService_991522132
 {
-    public class Class1
+    class IDBOperationService
     {
     }
 }
---
+++ FinalTourService_991522132/TourService_991522132.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace FinalTourService_991522132
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "TourService_991522132" in both code and config file together.
+    public class TourService_991522132 : ITourService_991522132
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
